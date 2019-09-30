0dd6d1051c4b0e23524451c927c8b5dc6c436ed0
ferdiaz93 <fernandodiaz1209@gmail.com>
2019-09-30 03:17:46 -0300

    script bd agregado

--- scripts/script-db.sql
+++ scripts/script-db.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE historialtweets;
 USE historialtweets;
-CREATE TABLE tweets (
-    id INT NOT NULL AUTO_INCREMENT,
-    busqueda VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id),
-);
+DROP TABLE IF EXISTS `tweets`;
+CREATE TABLE `tweets` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `titulo` varchar(30) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+LOCK TABLES `tweets` WRITE;
+UNLOCK TABLES;
